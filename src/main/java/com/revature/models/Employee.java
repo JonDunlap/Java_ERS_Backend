f59653501132e2c9ae6a70b97ddd425ffd23cd52
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-17 08:54:14 -0800

    Create variables and constructor for employee object

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,15 @@
 package com.revature.models;
 
 public class Employee {
+	// class values
+	public String email;
+	public String password;
+
+	// create constructor method
+	public Employee(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
 
 }
