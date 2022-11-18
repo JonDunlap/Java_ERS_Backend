3a468dcd5e4e673c4604768f4dce179c36d7b43d
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-18 08:11:05 -0800

    Add variables, constructor, and methods for ticket model

--- src/main/java/com/revature/models/Ticket.java
+++ src/main/java/com/revature/models/Ticket.java
@@ -2,4 +2,24 @@ package com.revature.models;
 
 public class Ticket {
 
+	// ticket variables
+	double amount;
+	String description;
+	String status = "pending";
+
+	// ticket constructor
+	public Ticket() {
+		super();
+	}
+
+	// public method to change default status variable
+	public void setStatus(String status) {
+		// TODO: possibly move this logic into the business logic class
+		if (status.toLowerCase() == "approved" || status.toLowerCase() == "denied") {
+			this.status = status;
+		} else {
+			throw new Error("Invalid status type, select either approved or denied.");
+		}
+	}
+
 }
