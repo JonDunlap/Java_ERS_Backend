a1e6685b442b8a9121f2458c6d68fac9df2d21b1
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-21 09:40:55 -0800

    Generate constructor, getters/setters, and methods for employee model

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -2,20 +2,94 @@ package com.revature.models;
 
 public class Employee {
 	// class values
-	public String email;
-	public String password;
+	private int id;
+	private String email;
+	private String password;
+	private boolean isManager;
 
-	// create constructor method
-	public Employee(String email, String password) {
+	public Employee(int id, String email, String password, boolean isManager) {
 		super();
+		this.id = id;
 		this.email = email;
+		this.password = password;
+		this.isManager = isManager;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	// override .toString() method
-	@Override
-	public String toString() {
-		return ("Email " + email + ", Password " + password);
+	public boolean isManager() {
+		return isManager;
 	}
 
+	public void setManager(boolean isManager) {
+		this.isManager = isManager;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + (isManager ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id != other.id)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (isManager != other.isManager)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", email=" + email + ", password=" + password + ", isManager=" + isManager + "]";
+	}
 }
