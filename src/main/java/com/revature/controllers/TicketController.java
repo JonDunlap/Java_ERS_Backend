0bc922a3e5e8c37b4997a5aec1ac8ed06814a594
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-21 09:07:30 -0800

    Create controllers for tickets and employees

---
+++ src/main/java/com/revature/controllers/TicketController.java
@@ -0,0 +1,11 @@
+package com.revature.controllers;
+
+import io.javalin.Javalin;
+
+public class TicketController implements Controller {
+	@Override
+	public void addRoutes(Javalin app) {
+		// TODO Auto-generated method stub
+		// add routes to GET, POST, PUT, DELETE? tickets
+	}
+}
