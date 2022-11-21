ca78e5f110869e3c6e0a3768a2babf7404648c53
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-21 13:36:00 -0800

    Add methods and handlers for employee controller

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -1,11 +1,31 @@
 package com.revature.controllers;
 
+import com.revature.models.Employee;
+import com.revature.services.EmployeeService;
+
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 
 public class EmployeeController implements Controller {
+	private EmployeeService employeeService = new EmployeeService();
+
+	private Handler getEmployee = ctx -> {
+		Employee employee = ctx.bodyAsClass(Employee.class);
+		employeeService.getEmployee(employee);
+
+		ctx.json(employee);
+		ctx.status(200);
+	};
+
+	private Handler addEmployee = ctx -> {
+		Employee employee = ctx.bodyAsClass(Employee.class);
+		employeeService.addEmployee(employee);
+		ctx.status(201);
+	};
+
 	@Override
 	public void addRoutes(Javalin app) {
-		// TODO Auto-generated method stub
-		// add routes to GET, POST, PUT employees
+		app.get("/employee", getEmployee);
+		app.post("/employee", addEmployee);
 	}
 }
