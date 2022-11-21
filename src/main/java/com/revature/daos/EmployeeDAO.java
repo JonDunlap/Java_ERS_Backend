181ac7c758be8e96e8d19f721631956b72138f65
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-21 12:47:27 -0800

    Add methods for employee DAO

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -1,5 +1,31 @@
 package com.revature.daos;
 
-public class EmployeeDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.revature.models.Employee;
+
+public class EmployeeDAO {
+	// ! DEBUG - temp. employees
+	private static List<Employee> employees = new ArrayList<>();
+
+	public EmployeeDAO() {
+		employees.add(new Employee(1, "test@email.com", "abc123", false));
+		employees.add(new Employee(2, "test2@email.com", "abc123", true));
+	}
+
+	// ? pass email/password or pass Employee?
+	public Employee getEmployee(String email, String password) {
+		for (Employee employee : employees) {
+			if (employee.getEmail().matches(email) && employee.getPassword().matches(password)) {
+				return employee;
+			}
+		}
+
+		throw new Error("Email and password do not match any employees");
+	}
+
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
 }
