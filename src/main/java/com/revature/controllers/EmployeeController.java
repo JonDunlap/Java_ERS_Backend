d50505933a1d3626ff05a9d43174b3fbdae912c8
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-29 12:49:43 -0800

    Complete employee controller methods

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import com.revature.exceptions.EmployeeExistsException;
 import com.revature.models.Employee;
 import com.revature.services.EmployeeService;
 
@@ -9,24 +12,48 @@ import io.javalin.http.Handler;
 public class EmployeeController implements Controller {
 	private EmployeeService employeeService = new EmployeeService();
 
-	private Handler getEmployee = ctx -> {
-		Employee employee = employeeService.getEmployee(ctx.bodyAsClass(Employee.class));
+	Handler getEmployees = ctx -> {
+		List<Employee> list = employeeService.getEmployees();
 
+		ctx.json(list);
+		ctx.status(200);
+	};
+
+	Handler getEmployee = ctx -> {
+		String idString = ctx.pathParam("id");
+
+		int id = 0;
+
+		try {
+			id = Integer.parseInt(idString);
+		} catch (NumberFormatException e) {
+			ctx.status(422);
+			return;
+		}
+
+		Employee employee = employeeService.getEmployee(id);
 		ctx.json(employee);
 		ctx.status(200);
 	};
 
-	private Handler addEmployee = ctx -> {
+	Handler addEmployee = ctx -> {
 		Employee employee = ctx.bodyAsClass(Employee.class);
-		employeeService.addEmployee(employee);
-		ctx.status(201);
+
+		try {
+			if (employeeService.addEmployee(employee)) {
+				ctx.status(200);
+			} else {
+				ctx.status(500);
+			}
+		} catch (EmployeeExistsException e) {
+			ctx.status(400);
+		}
 	};
 
-	// TODO - add logout route
-	// TODO - save employee to session storage
 	@Override
 	public void addRoutes(Javalin app) {
-		app.get("/employee", getEmployee);
+		app.get("/employee", getEmployees);
+		app.get("/employee/{id}", getEmployee);
 		app.post("/employee", addEmployee);
 	}
 }
