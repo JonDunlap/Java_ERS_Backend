8fd11a7bbe01cf2bb4e08158f9a9b768fd8fc0a3
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-29 12:35:57 -0800

    Add employee service functionality

--- src/main/java/com/revature/services/EmployeeService.java
+++ src/main/java/com/revature/services/EmployeeService.java
@@ -3,29 +3,29 @@ package com.revature.services;
 import java.util.List;
 
 import com.revature.daos.EmployeeDAO;
+import com.revature.daos.EmployeeDAOImpl;
+import com.revature.exceptions.EmployeeExistsException;
 import com.revature.models.Employee;
 
 public class EmployeeService {
-	private EmployeeDAO employeeDAO = new EmployeeDAO();
+	private EmployeeDAO employeeDAO = new EmployeeDAOImpl();
 
 	public List<Employee> getEmployees() {
 		return employeeDAO.getEmployees();
 	}
 
-	// TODO - move validation of employee to Service
-	// TODO - use .matches() to see if email already exists
-	// TODO - if email exists return error, else return employee object
-	// TODO - use employee object inside of session for session validation and
-	// TODO - ticket creation
-	public Employee getEmployee(Employee employee) {
-		return employeeDAO.getEmployee(employee);
+	public Employee getEmployee(int id) {
+		return employeeDAO.getEmployee(id);
 	}
 
-	// TODO - add validation that email doesn't exist already
-	public void addEmployee(Employee employee) {
-		// ! DEBUG - use salt/hash method for generating employee IDs
-		int newID = getEmployees().size() + 1;
-		employee.setId(newID);
-		employeeDAO.addEmployee(employee);
+	public boolean addEmployee(Employee employee) {
+		List<Employee> employees = getEmployees();
+
+		for (Employee emp : employees) {
+			if (emp.equals(employee)) {
+				throw new EmployeeExistsException("This employee exists already.");
+			}
+		}
+		return employeeDAO.addEmployee(employee);
 	}
 }
