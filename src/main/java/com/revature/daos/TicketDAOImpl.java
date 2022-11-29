8ce8ae5881ba5bbd258a8edd4328d3ad3c839d5a
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-29 14:44:26 -0800

    Finish implementing TicketDAO functionality

--- src/main/java/com/revature/daos/TicketDAOImpl.java
+++ src/main/java/com/revature/daos/TicketDAOImpl.java
@@ -14,7 +14,7 @@ import com.revature.utils.ConnectionUtil;
 public class TicketDAOImpl implements TicketDAO {
 
 	@Override
-	public boolean addTicket(Ticket ticket) {
+	public boolean addTicket(Ticket ticket, int employeeID) {
 		try (Connection connection = ConnectionUtil.getConnection()) {
 			String sql = "INSERT INTO tickets (amount, description, employee_id) VALUES" +
 					"(?,?,?);";
@@ -24,12 +24,7 @@ public class TicketDAOImpl implements TicketDAO {
 			int index = 1;
 			statement.setDouble(index++, ticket.getAmount());
 			statement.setString(index++, ticket.getDescription());
-			// TODO - use employee ID from session storage
-			if (ticket.getId() != 0) {
-				statement.setInt(index++, ticket.getId());
-			} else {
-				statement.setInt(index++, 0);
-			}
+			statement.setInt(index++, employeeID);
 
 			statement.execute();
 
@@ -54,15 +49,19 @@ public class TicketDAOImpl implements TicketDAO {
 
 			while (resultSet.next()) {
 				Ticket ticket = new Ticket();
+
 				ticket.setId(resultSet.getInt("ticket_id"));
 				ticket.setAmount(resultSet.getDouble("amount"));
 				ticket.setDescription(resultSet.getString("description"));
 				ticket.setStatus(resultSet.getString("status"));
 				ticket.setEmployeeID(resultSet.getInt("employee_id"));
 				ticket.setResolvingManagerID(resultSet.getInt("resolving_manager_id"));
+
+				tickets.add(ticket);
 			}
 
 			return tickets;
+
 		} catch (SQLException e) {
 			e.getStackTrace();
 			return null;
@@ -71,14 +70,58 @@ public class TicketDAOImpl implements TicketDAO {
 
 	@Override
 	public List<Ticket> getPendingTickets() {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.getConnection()) {
+
+			String sql = "SELECT * FROM tickets WHERE status='pending';";
+
+			Statement statement = connection.createStatement();
+
+			ResultSet resultSet = statement.executeQuery(sql);
+
+			List<Ticket> tickets = new ArrayList<>();
+
+			while (resultSet.next()) {
+				Ticket ticket = new Ticket();
+
+				ticket.setId(resultSet.getInt("ticket_id"));
+				ticket.setAmount(resultSet.getDouble("amount"));
+				ticket.setDescription(resultSet.getString("description"));
+				ticket.setStatus(resultSet.getString("status"));
+				ticket.setEmployeeID(resultSet.getInt("employee_id"));
+
+				tickets.add(ticket);
+			}
+
+			return tickets;
+
+		} catch (SQLException e) {
+			e.getStackTrace();
+			return null;
+		}
 	}
 
+	/*
+	 * Ticket is passed as parameter so that other parts of the ticket can be
+	 * updated later (status, description, amount, etc..)
+	 */
 	@Override
-	public boolean updateTicket(Ticket ticket) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean updateTicket(Ticket ticket, int managerID) {
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			String sql = "UPDATE tickets SET status=?, resolving_manager_id=" + managerID + " WHERE ticket_id=?;";
 
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			int index = 1;
+			statement.setString(index++, ticket.getStatus());
+			statement.setInt(index++, ticket.getId());
+
+			statement.execute();
+
+			return true;
+
+		} catch (SQLException e) {
+			e.getStackTrace();
+			return false;
+		}
+	}
 }
