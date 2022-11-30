91d3abb65f1de4e2549a24103a759ddabe45c198
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-11-30 12:30:21 -0800

    Remove unneeded comments

--- src/main/java/com/revature/services/TicketService.java
+++ src/main/java/com/revature/services/TicketService.java
@@ -9,24 +9,18 @@ import com.revature.models.Ticket;
 public class TicketService {
 	private TicketDAO ticketDAO = new TicketDAOImpl();
 
-	// TODO - get employeeID from SessionStorage
 	public boolean addTicket(Ticket ticket, int employeeID) {
 		return ticketDAO.addTicket(ticket, employeeID);
 	}
 
-	// TODO - get employeeID from SessionStorage
 	public List<Ticket> getEmployeeTickets(int employeeID) {
 		return ticketDAO.getEmployeeTickets(employeeID);
 	}
 
-	// TODO - verify user is manager from SessionStorage
-	// TODO - prevent managers from seeing their own tickets
 	public List<Ticket> getPendingTickets() {
 		return ticketDAO.getPendingTickets();
 	}
 
-	// TODO - get managerID from SessionStorage
-	// TODO - check ticket status is either approved/denied
 	public boolean updateTicket(Ticket ticket, int managerID) {
 		return ticketDAO.updateTicket(ticket, managerID);
 	}
