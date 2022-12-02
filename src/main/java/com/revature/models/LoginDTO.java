8aa3bc23ce748a981ca5fd24372dafb9bafb698b
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-02 11:31:10 -0800

    Add isManager field to LoginDTO

--- src/main/java/com/revature/models/LoginDTO.java
+++ src/main/java/com/revature/models/LoginDTO.java
@@ -3,11 +3,13 @@ package com.revature.models;
 public class LoginDTO {
 	public String email;
 	public String password;
+	public boolean isManager;
 
-	public LoginDTO(String email, String password) {
+	public LoginDTO(String email, String password, boolean isManager) {
 		super();
 		this.email = email;
 		this.password = password;
+		this.isManager = isManager;
 	}
 
 	public LoginDTO() {
